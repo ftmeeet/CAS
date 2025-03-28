a12655b2d1148adc6c184b5f807764f9f198bd89
ftmeeet <meet@uni-bremen.de>
2025-03-28 11:28:11 +0100

    modified orekit data, now locally installed with pip

--- Main.py
+++ Main.py
@@ -10,16 +10,16 @@ from org.orekit.utils import IERSConventions
 
 # Initialize Orekit VM
 orekit.initVM()
-setup_orekit_curdir("C:\Orekit\orekit-data-main\orekit-data-main")
+setup_orekit_curdir(from_pip_library=True)
 
 
-def propagate_tle_orekit(tle_line1, tle_line2, days, step=60):  # Using 6-sec step for accuracy
+def propagate_tle_orekit(tle_line1, tle_line2, days, step=1):  # Using 60-sec step for accuracy
     """
     Propagates the TLE using Orekit's SGP4 propagator and converts to TEME of Date.
     :param tle_line1: First line of TLE
     :param tle_line2: Second line of TLE
     :param days: Number of days to propagate
-    :param step: Time step in seconds (default: 6 seconds)
+    :param step: Time step in seconds (default: 60 seconds)
     :return: List of propagated state vectors (time, x, y, z, vx, vy, vz)
     """
     tle = TLE(tle_line1, tle_line2)
@@ -38,9 +38,9 @@ def propagate_tle_orekit(tle_line1, tle_line2, days, step=60):  # Using 6-sec st
         state = propagator.propagate(propagation_time)
         pv = state.getPVCoordinates(FramesFactory.getTEME())  # State vector in TEME of Epoch
         
-        # Convert the state vector to TEME of Date
-        teme_of_date = FramesFactory.getTEME().getTransformTo(itrf_frame, propagation_time)
-        pv_teme_date = teme_of_date.transformPVCoordinates(pv)  # Transform to TEME of Date
+        # Convert the state vector to TEME of Date (by transforming to ITRF and then to TEME)
+        teme_of_date_transform = FramesFactory.getTEME().getTransformTo(itrf_frame, propagation_time)
+        pv_teme_date = teme_of_date_transform.transformPVCoordinates(pv)  # Transform to TEME of Date
         
         position = pv_teme_date.getPosition()
         velocity = pv_teme_date.getVelocity()
@@ -55,7 +55,7 @@ def propagate_tle_orekit(tle_line1, tle_line2, days, step=60):  # Using 6-sec st
 
 # Example usage
 if __name__ == "__main__":
-    tle_line1 = "1 00005U 58002B   00179.78495062 +.00000023 +00000-0 +28098-4 0  4753"
+    tle_line1 = "1 00005U 58002B   00179.78495062 +.00000023 +00000-0 +28098-4 0  9994"
     tle_line2 = "2 00005  034.2682 348.7242 1859667 331.7664 019.3264 10.82419157413667"
     
     days = int(input("Enter the number of days to propagate: "))
